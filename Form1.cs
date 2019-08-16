cb6e90f6b57d03eafda153ebd6186778f3cf7372
Rosmel <51676094+Lemsor1@users.noreply.github.com>
2019-08-15 23:05:12 -0600

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Personajes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var Heroe1=new Heroe();
+
+            Heroe1.Nombre = "Superman";
+            Heroe1.Poder = "Volar";
+
+            var Heroe2 = new Heroe();
+
+            Heroe2.Nombre = "Flash";
+            Heroe2.Poder = "Super Velocidad";
+
+            var Heroe3 = new Heroe();
+
+            Heroe3.Nombre = "Hulk";
+            Heroe3.Poder = "Super Fuerza";
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            var Villano1 = new Villano();
+
+            Villano1.Nombre = "Guason";
+            Villano1.Poder = "Astucia";
+
+            var Villano2 = new Villano();
+
+            Villano2.Nombre = "Lex Luthor";
+            Villano2.Poder = "Dinero y inteligencia";
+
+            var Villano3 = new Villano();
+
+            Villano3.Nombre = "Zoom";
+            Villano3.Poder = "Velocidad";
+
+
+        }
+    }
+}
